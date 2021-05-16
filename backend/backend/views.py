25be6584383c532bb8e413d834412f42180ee08f
JHowell45 <jhowell45@googlemail.com>
2021-05-16 21:14:52 +0100

    Creating API for logging in.

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -1,10 +1,16 @@
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
+from jwt import decode
+from jwt.exceptions import (DecodeError, ExpiredSignatureError,
+                            InvalidSignatureError, InvalidTokenError)
 from rest_framework import status
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from .utils import generate_tokens
 
 
 class HelloWorld(APIView):
@@ -75,4 +81,100 @@ class Login(APIView):
     permission_classes = (IsAuthenticated,)
 
     def post(self, request):
-        return Response("Authentication Successful.")
+        access_token, refresh_token = generate_tokens(request.user)
+        if access_token is None or refresh_token is None:
+            return Response(
+                {"error": "Something went wrong!"},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
+        response = {
+            "access_token": access_token,
+            "expires_in": 3600,
+            "token_type": "bearer",
+            "refresh_token": refresh_token,
+        }
+
+        return Response(response)
+
+
+class LoginRefresh(APIView):
+    def post(self, request):
+        try:
+            data = request.data
+            try:
+                refresh_token = data["refresh_token"]
+            except KeyError:
+                return Response(
+                    {"error": "Refresh token required!"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
+            # Validating the refresh token
+            try:
+                decoded_refresh_token_payload = decode(
+                    refresh_token, settings.SECRET_KEY, algorithms="HS256"
+                )
+            except InvalidSignatureError:
+                return Response(
+                    {"error": "Invalid Signature, Token tampered!"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+            except ExpiredSignatureError:
+                return Response(
+                    {"error": "Token expired"}, status=status.HTTP_400_BAD_REQUEST
+                )
+            except (InvalidTokenError, DecodeError):
+                return Response(
+                    {"error": "Invalid Token"}, status=status.HTTP_400_BAD_REQUEST
+                )
+
+            try:
+                if not (decoded_refresh_token_payload["type"] == "refresh"):
+                    return Response(
+                        {"error": "Invalid token type"},
+                        status=status.HTTP_400_BAD_REQUEST,
+                    )
+
+                user_name = decoded_refresh_token_payload["username"]
+            except KeyError:
+                return Response(
+                    {"error": "Token tampered!"}, status=status.HTTP_400_BAD_REQUEST
+                )
+
+            # Getting user object from database
+            try:
+                current_user = User.objects.get(username=user_name)
+            except User.DoesNotExist:
+                return Response(
+                    {"error": "User Doesn't exist"}, status=status.HTTP_400_BAD_REQUEST
+                )
+            except User.MultipleObjectsReturned:
+                return Response(
+                    {"error": "Fatal! Multiple users with the same " "user name exist"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
+            # Generating tokens
+            access_token, refresh_token = generate_tokens(current_user)
+
+            if access_token is None or refresh_token is None:
+                return Response(
+                    {"error": "Something went wrong"},
+                    status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                )
+
+            response = {
+                "access_token": access_token,
+                "expires_in": 3600,
+                "token_type": "bearer",
+                "refresh_token": refresh_token,
+            }
+
+            return Response(response)
+
+        except Exception as er:
+            print(er)
+            return Response(
+                "Oops!, Some thing went wrong while handling your request",
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
