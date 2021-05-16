c3bce65c6a0395781fe4ec3ad420b04bf6c93cea
JHowell45 <jhowell45@googlemail.com>
2021-05-16 19:47:52 +0100

    Updated URLs.

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from . import views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("hello_world", views.HelloWorld.as_view()),
+    path("login/register", views.Register.as_view()),
 ]
