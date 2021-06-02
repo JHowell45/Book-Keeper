81db32d8142f1ae9cbcd13a9719a70f7083bcad9
JHowell45 <jhowell45@googlemail.com>
2021-06-02 19:35:02 +0100

    Create base project.

--- book_keeper/book_keeper/urls.py
+++ book_keeper/book_keeper/urls.py
@@ -1,4 +1,4 @@
-"""bookkeeper URL Configuration
+"""book_keeper URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
